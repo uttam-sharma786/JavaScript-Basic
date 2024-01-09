882c8e941d025a020baea80cf40714c3aad19783
uttam-sharma786 <uttamsharma.8560@gmail.com>
2024-01-09 13:29:19 +0530

    few change

---
+++ intervier-question/Basic-question-2.js
@@ -0,0 +1,9 @@
+//Q2- WHat is function Expression
+/* This function does not have a name, which is called by ananymous funciton */
+const square = function (num) {
+    return num*num
+};
+console.log(square(8));
+
+// what is difference between function Declaration and function Expression
+// function Expression just assign to a vairable here const square
