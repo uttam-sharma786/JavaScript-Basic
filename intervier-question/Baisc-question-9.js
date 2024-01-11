93d1a595f784dcb68e0853071ef1f33b2a75823a
uttam-sharma786 <uttamsharma.8560@gmail.com>
2024-01-11 11:25:36 +0530

    11/1/2024

---
+++ intervier-question/Baisc-question-9.js
@@ -0,0 +1,6 @@
+// Callback function
+// When we are passing a function inside of another function, and this function is manipulated inside of this one.
+// so it can be event listeners, it can be function like map, filter, reduce or setTimeout
+document.addEventListener("click", function(params){
+
+})
