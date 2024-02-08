913ad5d621cb32cd029fb6e69d98254e660d45f9
uttam-sharma786 <uttamsharma.8560@gmail.com>
2024-02-08 11:05:26 +0530

    different between == and ====

---
+++ intervier-question/equals.js
@@ -0,0 +1,18 @@
+console.log(2 == 2.0);
+
+
+console.log("" === "");
+
+let myVar = {}
+let myVarTwo = {}
+// myVarTwo = myVar // true
+
+console.log({} === {});
+
+
+console.log(myVar == myVarTwo);
+/*
+== operator (Equality
+=== operator (identity)    
+    )
+*/
