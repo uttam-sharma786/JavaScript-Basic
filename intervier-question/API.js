76c4e5b8ede834da22c665bc9ac0e491d6fcf325
uttam-sharma786 <uttamsharma.8560@gmail.com>
2024-02-08 12:35:58 +0530

    update inside API

--- intervier-question/API.js
+++ intervier-question/API.js
@@ -6,6 +6,8 @@ const api = fetch('https://api.chucknorris.io/jokes/random')
     // console.log("Data is : ", data);
     var joke = data.value;
     console.log("JOKe :", joke);
+    var id = data.id;
+    console.log("id",id);
 })
 
 .catch(error =>{
